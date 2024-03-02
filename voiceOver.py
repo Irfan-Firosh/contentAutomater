66eea67b6bc5ceec859f0f9da0cb423c43750f27
Irfan Firosh <irfan.firosh@gmail.com>
2024-03-02 05:18:03 -0500

    Incorporated major changes: Added screenshots to main, made voicover filename changes

--- voiceOver.py
+++ voiceOver.py
@@ -4,16 +4,8 @@ from gtts import gTTS
 voiceoverDirectory = "voiceovers"
 
 #Creating a voiceover function
-def createVoiceover(comment_id, inp):
-    path = f'{voiceoverDirectory}/comment-{comment_id}.mp3'
+def createVoiceover(comment_id, inp, post_id):
+    path = f'{voiceoverDirectory}/post-{post_id}-comment-{comment_id}.mp3'
     tts = gTTS(text=inp, lang='en')
     tts.save(path)
     return path
-
-#def createVoiceover(comment_id, text):
- #   engine = pyttsx3.init()
-  #  path = f'{voiceoverDirectory}/comment-{comment_id}.mp3'
-   # engine.setProperty('voice', 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_ZIRA_11.0')
-    #engine.save_to_file(text, path)
-    #engine.runAndWait()
-    #return path
--- wifiConnector.py
+++ wifiConnector.py
@@ -4,5 +4,8 @@ def connect(router_name):
     #os.system('cmd /c "netsh wlan show networks"')
     os.system(f'''cmd /c "netsh wlan connect name={router_name}"''')
 
+user = "1"
 
+u = user + "2"
 
+print(u)
