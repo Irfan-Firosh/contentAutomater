0ed4589b261e06caae84a7bde007236b95245fa2
Irfan Firosh <irfan.firosh@gmail.com>
2024-03-12 06:03:13 -0400

    Added uploading to yt auth

---
+++ ytUploader.py
@@ -0,0 +1,74 @@
+import os
+
+import http.client
+import httplib2
+from google.oauth2.credentials import Credentials
+from google.auth.transport.requests import Request
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from googleapiclient.http import MediaFileUpload
+from google_auth_oauthlib.flow import InstalledAppFlow
+from oauth2client.tools import run_flow
+
+
+
+client_secrets_file = "yt_0Auth.json"
+cred_file = "token.json"
+
+
+httplib2.RETRIES = 1
+
+MAX_RETRIES = 10
+RETRIABLE_EXCEPTIONS = (httplib2.HttpLib2Error, IOError, http.client.NotConnected,
+  http.client.IncompleteRead, http.client.ImproperConnectionState,
+  http.client.CannotSendRequest, http.client.CannotSendHeader,
+  http.client.ResponseNotReady, http.client.BadStatusLine)
+RETRIABLE_STATUS_CODES = [500, 502, 503, 504]
+CLIENT_SECRET = 'yt_0Auth.json'
+SCOPES = ['https://www.googleapis.com/auth/youtube.upload']
+API_SERVICE_NAME = 'youtube'
+API_VERSION = 'v3'
+PRIVACY_STATUS = 'public'
+
+# authorize request and store credentials
+def get_authenticated_service():
+    credentials = None
+    if (os.path.exists(cred_file)):
+        credentials = Credentials.from_authorized_user_file(cred_file, SCOPES)
+    
+    if credentials is None or not credentials.valid:
+        if credentials and credentials.expired and credentials.refresh_token:
+            credentials.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(client_secrets_file, SCOPES)
+            credentials = flow.run_local_server(port=0)
+            with open(cred_file, "w") as token:
+                token.write(credentials.to_json())
+    return build(API_SERVICE_NAME, API_VERSION, credentials=credentials)
+
+def initalize_upload(file, options):
+    youtube = get_authenticated_service()
+    try:
+        insert_request = youtube.videos().insert(
+            part="snippet,status",
+            body={
+              "snippet": {
+                "title": options["title"],
+                "description": options["description"],
+                "tags": options["tags"],
+                "categoryId": options["category"]
+              },
+              "status": {
+                "privacyStatus": options["status"]
+              }
+            },
+            media_body=MediaFileUpload(file)
+        )
+        response = insert_request.execute()
+        return response
+    except HttpError as e:
+        print(f"An HTTP error {e.resp.status} occurred:\n{e.content}")
+        return None
+
+import config
+initalize_upload("finalVideos/1bc6z75.mp4", config.youtube)
