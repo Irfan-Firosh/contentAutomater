0d5c65dc6c11f48413e065c8e64167c2ec73c021
Irfan Firosh <irfan.firosh@gmail.com>
2024-03-11 20:51:49 -0400

    Edited to handle error cases

--- screenShot.py
+++ screenShot.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import NoSuchElementException
 import time
 
 
@@ -24,30 +25,32 @@ def takeSS(url, post_id, comment_id, option: int):
         driver.close()
         print('Failed for ss for post: ' + post_id + ' comment: ' + comment_id)
 def setupDriver(url: str):
-    options = webdriver.FirefoxOptions()
-    options.headless = False
-    options.mobile_options = False
-    options.set_preference("dom.popup_maximum", 0)
-    options.set_preference("privacy.popups.showBrowserMessage", False)
-    options.set_preference("dom.disable_open_during_load", False)
-    print(options)
-    driver = webdriver.Firefox(options=options)
-    wait = WebDriverWait(driver, 10)
-    driver.set_window_size(width=screenWidth, height= screenHeight)
-    driver.get(url)
-    return driver, wait
+    try:
+        options = webdriver.FirefoxOptions()
+        options.headless = False
+        options.mobile_options = False
+        driver = webdriver.Firefox(options=options)
+        wait = WebDriverWait(driver, 10)
+        driver.set_window_size(width=screenWidth, height= screenHeight)
+        driver.get(url)
+        return driver, wait
+    except:
+        print("Error setting up driver")
 
 def takeTitleScreenshot(driver, wait, id):
-    handle = By.ID
-    name = f"t3_{id}"
-    element = wait.until(EC.presence_of_element_located((handle, name)))
-    driver.execute_script("window.focus();")
-    ssName = f"{screenshotDir}/{id}_post.png"
-    file = open(ssName, "wb")
-    file.write(element.screenshot_as_png)
-    file.close()
+    try:
+        handle = By.ID
+        name = f"t3_{id}"
+        element = wait.until(EC.presence_of_element_located((handle, name)))
+        driver.execute_script("window.focus();")
+        ssName = f"{screenshotDir}/{id}_post.png"
+        file = open(ssName, "wb")
+        file.write(element.screenshot_as_png)
+        file.close()
+    except:
+        print("Screenshot error post: " + id)
 
-def takeCommentScreenshot(driver, wait, id):
+def takeCommentScreenshot(driver, wait, id,):
     try:
         handle = By.ID
         name = f't1_{id}'
@@ -73,10 +76,10 @@ def cancelLoginPopup(driver):
             if src:
                 popup = iframe
                 break
-
-        print(popup.get_attribute('src'))
         driver.switch_to.frame(popup)
         driver.find_element("xpath", '//div[@role="button"]').click()
         driver.switch_to.default_content()
-    except Exception as e:
+    except NoSuchElementException:
         print("No Login Found")
+    except:
+        print("Error with cancel login")
