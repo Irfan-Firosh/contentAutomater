30620043b714490e42e4d8029d1936f388df6ee0
Irfan-Firosh <86122457+Irfan-Firosh@users.noreply.github.com>
2024-03-12 23:33:47 -0400

    Created sample config.py file

---
+++ config.py
@@ -0,0 +1,18 @@
+praw_client_id ="YOUR CLIENT ID"
+praw_client_secret = "YOUR CLIENT USERNAME"
+praw_password="YOUR PASSWORD"
+praw_username="YOUR USERNAME"
+agent = "YOUR AGENT"
+
+youtube = {
+    "title": "YOUR TITLE",
+    "description": "YOUR DESCRIPTION",
+    "tags": [], # Add any tags as a string list
+    "category": "23", # Category is an int
+    "status": "private"  # "public", "private", or "unlisted"
+}
+
+# Do not change these variables
+success = {}
+failPost = []
+failComment = []
